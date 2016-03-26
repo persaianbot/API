c2e4b747a50a21b372d3a998c77057f1850af178
Arain-Soft <ArianGhasempour40@Gmail.com>
2016-03-26 18:09:31 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = " "--توکن بوت را درون " قرار دهید --
+local bot_api_key = "188657706:AAGluIz5nSxvis6gfZ-ohU6zBhAoH9mShOY"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[
@@ -21,21 +21,21 @@ hi :)
 - _test_
 
 /hyper [[text](url)](url)
-- [BeatBot](BeatBot.ir)
+- [TekTeamBot](Telegram?me/TekTeamBot)
 
 *EXAMPLE*
 
 /hyper [`Hi`]
 [*I'm a api bot* ]
 [_BeatBot Team created me_]
-[[BeatBot Channel](test)][(http://telegram.me/beatbot_team)]
+[[TekBot Channel](test)][(http://telegram.me/TekBot_Team)]
 
 *RESULT*
 
 `Hi`
 *I'm a api bot* 
-_BeatBot Team created me_
-[BeatBot Channel](http://telegram.me/beatbot_team)
+_TekBot  Team created me_
+[TekBot Channel](http://telegram.me/TekBot_team)
 
 *OTHER*
 
@@ -57,7 +57,7 @@ _TNX TO USE ME :)_
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {94746365}-- put your id here
+  local admins = {192695150}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
